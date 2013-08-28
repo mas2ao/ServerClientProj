08e8c45b44ad63133233fedd4f8f27ad3c158147
YuKill <sir.yukio@gmail.com>
2013-08-28 10:37:19 -0300

    Intermediary machine created

---
+++ inter/int_machine.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "../data/funcionario.h"
+#include "../data/req.h"
+
+int main() {
+
+}
